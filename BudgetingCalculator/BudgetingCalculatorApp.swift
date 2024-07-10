495700c6c160cb5eb3cff64da5e662c019d855f3
embunazzahra <dhauembunazzahra@gmail.com>
2024-07-10 08:07:38 +0700

    Initial Commit

---
+++ BudgetingCalculator/BudgetingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetingCalculatorApp.swift
+//  BudgetingCalculator
+//
+//  Created by Dhau Embun Azzahra on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetingCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BudgetingCalculator
+//
+//  Created by Dhau Embun Azzahra on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
