15c44c4274e41dbf86af4ac203b9e071b5d1c301
embunazzahra <dhauembunazzahra@gmail.com>
2024-07-10 08:23:33 +0700

    add .gitignore file

---
+++ BudgetingCalculator/CalcView.swift
@@ -0,0 +1,18 @@
+//
+//  CalcView.swift
+//  BudgetingCalculator
+//
+//  Created by Dhau Embun Azzahra on 10/07/24.
+//
+
+import SwiftUI
+
+struct CalcView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalcView()
+}
