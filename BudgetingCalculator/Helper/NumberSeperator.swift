82dff6e839437e0d3d798a981be4a03992a35b23
Michael Kostaman <kostamanmichael@gmail.com>
2024-07-12 18:48:59 +0700

    add revert format iin numberformatter

--- BudgetingCalculator/Helper/NumberSeperator.swift
+++ BudgetingCalculator/Helper/NumberSeperator.swift
@@ -8,17 +8,26 @@
 import Foundation
 
 class NumberSeperator {
+    private let numberFormatter: NumberFormatter
+    
+    init() {
+        self.numberFormatter = NumberFormatter()
+        self.numberFormatter.numberStyle = .decimal
+        self.numberFormatter.groupingSeparator = "."
+        self.numberFormatter.locale = Locale(identifier: "id_ID")
+    }
+    
     public func formatNumber(_ number: String) -> String {
         let cleanedNumber = number.replacingOccurrences(of: ".", with: "")
         guard let numberValue = Int(cleanedNumber) else {
             return number
         }
         
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.groupingSeparator = "."
-        numberFormatter.locale = Locale(identifier: "id_ID")
-        
         return numberFormatter.string(from: NSNumber(value: numberValue)) ?? number
+    }
+    
+    public func revertFormat(_ formattedNumber: String) -> String {
+        let cleanedNumber = formattedNumber.replacingOccurrences(of: numberFormatter.groupingSeparator, with: "")
+        return cleanedNumber
     }
 }
