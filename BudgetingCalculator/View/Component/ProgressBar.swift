a035a40faa9605582164d9b64b8b6b68c08da567
Michael Kostaman <kostamanmichael@gmail.com>
2024-07-12 16:04:07 +0700

    add progressbar in calculator

---
+++ BudgetingCalculator/View/Component/ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.swift
+//  BudgetingCalculator
+//
+//  Created by Michael Varian Kostaman on 12/07/24.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
---
+++ BudgetingCalculator/View/Component/ProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarView.swift
+//  BudgetingCalculator
+//
+//  Created by Michael Varian Kostaman on 12/07/24.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBarView()
+}
