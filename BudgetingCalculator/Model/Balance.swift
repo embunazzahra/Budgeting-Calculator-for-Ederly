a270a09abf4780abd6e0be8172c48971a8aeebcf
Michael Kostaman <kostamanmichael@gmail.com>
2024-07-12 10:44:30 +0700

    fix dashboard design and add edit balance function

---
+++ BudgetingCalculator/Model/Balance.swift
@@ -0,0 +1,8 @@
+//
+//  Balance.swift
+//  BudgetingCalculator
+//
+//  Created by Michael Varian Kostaman on 11/07/24.
+//
+
+import Foundation
---
+++ BudgetingCalculator/View/EditBalanceView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBalance.swift
+//  BudgetingCalculator
+//
+//  Created by Michael Varian Kostaman on 11/07/24.
+//
+
+import SwiftUI
+
+struct EditBalance: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBalance()
+}
