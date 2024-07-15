cd20589bf8574f0ea25199a57de417a53e3996ff
embunazzahra <dhauembunazzahra@gmail.com>
2024-07-15 11:14:19 +0700

    hapusunused comment

--- BudgetingCalculator/View/AllHistoryView.swift
+++ BudgetingCalculator/View/AllHistoryView.swift
@@ -17,53 +17,49 @@ struct AllHistoryView: View {
     var body: some View {
         NavigationView {
             VStack{
-                //Calendar
-//                ScrollView(.vertical,showsIndicators: false){
-                    LazyVStack(spacing:15,pinnedViews: [.sectionHeaders]){
-                        Section{
-                            ScrollView(.horizontal,showsIndicators: false){
-                                HStack(spacing:10){
-                                    ForEach(modelView.currentWeek, id:\.self){ day in
-                                        VStack(spacing: 10){
+                LazyVStack(spacing:15,pinnedViews: [.sectionHeaders]){
+                    Section{
+                        ScrollView(.horizontal,showsIndicators: false){
+                            HStack(spacing:10){
+                                ForEach(modelView.currentWeek, id:\.self){ day in
+                                    VStack(spacing: 10){
+                                        
+                                        Text(modelView.extractDate(date: day, format: "EEE").prefix(1))
+                                            .font(.system(size: 14))
+                                            .fontWeight(.semibold)
+                                        
+                                        ZStack{
+                                            Circle()
+                                                .fill(.yellowFFCF23)
+                                                .frame(width: 26,height: 26)
+                                                .opacity(modelView.isToday(date: day) ? 1 : 0)
                                             
-                                            Text(modelView.extractDate(date: day, format: "EEE").prefix(1))
+                                            Text(modelView.extractDate(date: day, format: "dd"))
                                                 .font(.system(size: 14))
-                                                .fontWeight(.semibold)
-                                            
-                                            ZStack{
-                                                Circle()
-                                                    .fill(.yellowFFCF23)
-                                                    .frame(width: 26,height: 26)
-                                                    .opacity(modelView.isToday(date: day) ? 1 : 0)
-                                                
-                                                Text(modelView.extractDate(date: day, format: "dd"))
-                                                    .font(.system(size: 14))
-                                                    .fontWeight(.regular)
-                                            }
-                                            
-                                            
-                                            
-                                            
+                                                .fontWeight(.regular)
                                         }
-                                        .frame(width: 45, height: 90)
-                                        .background(
-                                            Color.clear
-                                        )
-                                        .onTapGesture {
-                                            withAnimation{
-                                                modelView.selectedDate = day
-                                            }
-                                        }
-                                      
+                                        
+                                        
+                                        
+                                        
                                     }
+                                    .frame(width: 45, height: 90)
+                                    .background(
+                                        Color.clear
+                                    )
+                                    .onTapGesture {
+                                        withAnimation(.snappy){
+                                            modelView.selectedDate = day
+                                        }
+                                    }
+                                    
                                 }
-//                                .background(.yellowFFCF23.opacity(0.3))
-                                .padding(.horizontal)
                             }
+                            .padding(.horizontal)
                         }
                     }
-                    .background(.yellowFFCF23.opacity(0.3))
-//                }
+                }
+                .background(.yellowFFCF23.opacity(0.3))
                 
                 
                 
